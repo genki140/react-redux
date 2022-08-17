d1c6083dbf3f6cf0de21ff8312017505570c8b86
石丸 元気 <genki140@gmail.com>
2022-08-17 16:48:25 +0900

    いったんコミット

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
 import App from './viess/App';
 import reportWebVitals from './reportWebVitals';
 
@@ -12,9 +11,6 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// ここを見ながら自分で実装して見る？
-// https://stackoverflow.com/questions/69953377/react-router-v6-how-to-use-navigate-redirection-in-axios-interceptor/70012117#70012117
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
